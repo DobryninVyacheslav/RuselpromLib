2f522ddb6c686099240d2fe2193507d325c6caa7
v.dobrinin <v.dobrinin@DESKTOP-009.rms.local>
2020-02-06 13:47:07 +0300

    Added FlexDimensions.java
    Flexible dimensions have been added in order to use them when inserting
    a flexible component into an assembly.

---
+++ RuselpromLib/ant/build/src/ru/ruselprom/argument/assembly/RefCoordSystems.java
@@ -0,0 +1,27 @@
+package ru.ruselprom.argument.assembly;
+
+public class RefCoordSystems {
+	private String compCsysName;
+	private String asmCsysName;
+	
+	public RefCoordSystems(String compCsysName, String asmCsysName) {
+		this.compCsysName = compCsysName;
+		this.asmCsysName = asmCsysName;
+	}
+
+	public String getCompCsysName() {
+		return compCsysName;
+	}
+
+	public void setCompCsysName(String compCsysName) {
+		this.compCsysName = compCsysName;
+	}
+
+	public String getAsmCsysName() {
+		return asmCsysName;
+	}
+
+	public void setAsmCsysName(String asmCsysName) {
+		this.asmCsysName = asmCsysName;
+	}	
+}
